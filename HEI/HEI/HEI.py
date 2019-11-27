3f4592498b1c260a8f06354b77a885ab9f38df71
Grace-Shearrer <grace.shearrer@gmail.com>
2019-11-27 14:26:45 -0500

    finally not creating doubles

--- HEI/HEI/HEI.py
+++ HEI/HEI/HEI.py
@@ -112,6 +112,7 @@ def BCP(diet_df, arglist):
 
 #### conversions #######
 def cup2oz(cup):
+    cup=cup.astype('float32')
     oz=cup*8
     return(oz)
 
@@ -131,93 +132,93 @@ def splitter(DF):
     df={'DF_child':DF_child, 'DF_young':DF_young, 'DF_infant':DF_infant}
     return(df)
 
-def file_org(infile, arglist, important):
-    if arglist['OPTS'] == False:
-        arglist['OPTS']=[arglist['NAMES']]
-        file_dict = {"set_04": {"%s"%arglist['NAMES']:{}}, "set_09": {"%s"%arglist['NAMES']: {}}}
-        file_dict['set_04']["%s"%arglist['NAMES']]["files"] = [x for x in glob.glob(os.path.join(infile,'*04.txt')) if "%s"%arglist['NAMES'] in x]
-        file_dict['set_09']["%s"%arglist['NAMES']]["files"] = [x for x in glob.glob(os.path.join(infile,'*09.txt')) if "%s"%arglist['NAMES'] in x]
-        arglist['OPTS']=[arglist['NAMES']]
-
-    else:
-        file_dict={"set_04":{},"set_09":{}}
-        for item in arglist['OPTS']:
-            print('this is the item %s'%item)
-            file_dict["set_04"][item]= {}
-            file_dict["set_09"][item]= {}
-
-        for key,value in file_dict.items():
-            keys=list(file_dict[key].keys())
-            for k in keys:
-                print(k)
-                file_dict['set_04']["%s"%k]["files"] = [x for x in glob.glob(os.path.join(infile,'*04.txt')) if '%s'%k in x]
-                file_dict['set_09']["%s"%k]["files"] = [x for x in glob.glob(os.path.join(infile,'*09.txt')) if '%s'%k in x]
-
-    return_dict={}
-    for ki in arglist['OPTS']:
-        temp_list = []
-        print(ki)
-        for file in file_dict["set_04"]["%s"%ki]["files"]:
-            temp_df =  pd.read_csv(file, sep="\t", encoding='latin1')
-            temp_df=temp_df.drop([0])
-            for val in temp_df["Participant ID"]:
-                _id = str(val).lstrip("0").split("_")[0]
-                temp_df.replace(val, _id, inplace=True)
-            temp_list.append(temp_df)
-        dfm4_original = pd.concat(temp_list, ignore_index=True)
-        print("Final dataframe size: ", dfm4_original.shape)
-        dfm4_original = dfm4_original.sort_values(by="Participant ID")
-
-        for col in dfm4_original:
-            if dfm4_original[col].dtype == np.object_:
-                dfm4_original[col] = (dfm4_original[col].replace(',','.', regex=True))
-
-        temp_list = []
-        for file in file_dict["set_09"]["%s"%ki]["files"]:
-            temp_df = pd.read_csv(file,encoding='latin1', sep="\t")
-            temp_df=temp_df.drop([0])
-            for val in temp_df["Participant ID"]:
-                _id = str(val).strip("0").strip(".").split("_")[0]
-                temp_df.replace(val, _id, inplace=True)
-            temp_list.append(temp_df)
-        dfm9_original = pd.concat(temp_list, ignore_index=True)
-        dfm9_original = dfm9_original.sort_values(by="Participant ID")
-
-        concat_filepath = os.path.join(arglist['SAVE'],'%s_dataset4.csv'%(ki))
-        dfm4_original.to_csv(concat_filepath, index=False, sep=",", header=True)
-
-        concat_filepath = os.path.join(arglist['SAVE'],'%s_dataset9.csv'%(ki))
-        dfm9_original.to_csv(concat_filepath, index=False, sep=",", header=True)
-
-        b=list(dfm9_original['Participant ID'])
-        a=list(dfm4_original['Participant ID'])
-
-        common=list(set(a) & set(b))
-        missmatch = list(set(a)-set(b))
-
-        common4=dfm4_original[dfm4_original['Participant ID'].isin(common)]
-        common9=dfm9_original[dfm9_original['Participant ID'].isin(common)]
-
-        mm4=dfm4_original[dfm4_original['Participant ID'].isin(missmatch)]
-        mm9=dfm9_original[dfm9_original['Participant ID'].isin(missmatch)]
-
-        total_df = common4.merge(common9.drop_duplicates(subset=['Project Abbreviation','Date of Intake']), how='left')
-        total_df=total_df.dropna(axis=1, how='all')
-        complete=total_df[total_df.columns.intersection(important)].dropna()
-        cind=list(complete.index)
-        complete_df=total_df[total_df.index.isin(cind)]
-
-        missing_df=total_df[~total_df.index.isin(cind)]
-        missing_df=missing_df.append(mm4)
-        missing_df=missing_df.append(mm9)
-
-        concat_filepath = os.path.join(arglist['SAVE'],'%s_BCP_datasetTOTAL.csv'%(ki))
-        complete_df.to_csv(concat_filepath, index=False, sep=",", header=True)
-
-        missing_filepath = os.path.join(arglist['SAVE'],'%s_BCP_datasetMissing.csv'%(ki))
-        missing_df.to_csv(missing_filepath, index=False, sep=",", header=True)
-        return_dict[ki]=complete_df
-    return(return_dict)
+# def file_org(infile, arglist, important):
+#     if arglist['OPTS'] == False:
+#         arglist['OPTS']=[arglist['NAMES']]
+#         file_dict = {"set_04": {"%s"%arglist['NAMES']:{}}, "set_09": {"%s"%arglist['NAMES']: {}}}
+#         file_dict['set_04']["%s"%arglist['NAMES']]["files"] = [x for x in glob.glob(os.path.join(infile,'*04.txt')) if "%s"%arglist['NAMES'] in x]
+#         file_dict['set_09']["%s"%arglist['NAMES']]["files"] = [x for x in glob.glob(os.path.join(infile,'*09.txt')) if "%s"%arglist['NAMES'] in x]
+#         arglist['OPTS']=[arglist['NAMES']]
+#
+#     else:
+#         file_dict={"set_04":{},"set_09":{}}
+#         for item in arglist['OPTS']:
+#             print('this is the item %s'%item)
+#             file_dict["set_04"][item]= {}
+#             file_dict["set_09"][item]= {}
+#
+#         for key,value in file_dict.items():
+#             keys=list(file_dict[key].keys())
+#             for k in keys:
+#                 print(k)
+#                 file_dict['set_04']["%s"%k]["files"] = [x for x in glob.glob(os.path.join(infile,'*04.txt')) if '%s'%k in x]
+#                 file_dict['set_09']["%s"%k]["files"] = [x for x in glob.glob(os.path.join(infile,'*09.txt')) if '%s'%k in x]
+#
+#     return_dict={}
+#     for ki in arglist['OPTS']:
+#         temp_list = []
+#         print(ki)
+#         for file in file_dict["set_04"]["%s"%ki]["files"]:
+#             temp_df =  pd.read_csv(file, sep="\t", encoding='latin1')
+#             temp_df=temp_df.drop([0])
+#             for val in temp_df["Participant ID"]:
+#                 _id = str(val).lstrip("0").split("_")[0]
+#                 temp_df.replace(val, _id, inplace=True)
+#             temp_list.append(temp_df)
+#         dfm4_original = pd.concat(temp_list, ignore_index=True)
+#         print("Final dataframe size: ", dfm4_original.shape)
+#         dfm4_original = dfm4_original.sort_values(by="Participant ID")
+#
+#         for col in dfm4_original:
+#             if dfm4_original[col].dtype == np.object_:
+#                 dfm4_original[col] = (dfm4_original[col].replace(',','.', regex=True))
+#
+#         temp_list = []
+#         for file in file_dict["set_09"]["%s"%ki]["files"]:
+#             temp_df = pd.read_csv(file,encoding='latin1', sep="\t")
+#             temp_df=temp_df.drop([0])
+#             for val in temp_df["Participant ID"]:
+#                 _id = str(val).strip("0").strip(".").split("_")[0]
+#                 temp_df.replace(val, _id, inplace=True)
+#             temp_list.append(temp_df)
+#         dfm9_original = pd.concat(temp_list, ignore_index=True)
+#         dfm9_original = dfm9_original.sort_values(by="Participant ID")
+#
+#         concat_filepath = os.path.join(arglist['SAVE'],'%s_dataset4.csv'%(ki))
+#         dfm4_original.to_csv(concat_filepath, index=False, sep=",", header=True)
+#
+#         concat_filepath = os.path.join(arglist['SAVE'],'%s_dataset9.csv'%(ki))
+#         dfm9_original.to_csv(concat_filepath, index=False, sep=",", header=True)
+#
+#         b=list(dfm9_original['Participant ID'])
+#         a=list(dfm4_original['Participant ID'])
+#
+#         common=list(set(a) & set(b))
+#         missmatch = list(set(a)-set(b))
+#
+#         common4=dfm4_original[dfm4_original['Participant ID'].isin(common)]
+#         common9=dfm9_original[dfm9_original['Participant ID'].isin(common)]
+#
+#         mm4=dfm4_original[dfm4_original['Participant ID'].isin(missmatch)]
+#         mm9=dfm9_original[dfm9_original['Participant ID'].isin(missmatch)]
+#
+#         total_df = common4.merge(common9.drop_duplicates(subset=['Project Abbreviation','Date of Intake']), how='left')
+#         total_df=total_df.dropna(axis=1, how='all')
+#         complete=total_df[total_df.columns.intersection(important)].dropna()
+#         cind=list(complete.index)
+#         complete_df=total_df[total_df.index.isin(cind)]
+#
+#         missing_df=total_df[~total_df.index.isin(cind)]
+#         missing_df=missing_df.append(mm4)
+#         missing_df=missing_df.append(mm9)
+#
+#         concat_filepath = os.path.join(arglist['SAVE'],'%s_BCP_datasetTOTAL.csv'%(ki))
+#         complete_df.to_csv(concat_filepath, index=False, sep=",", header=True)
+#
+#         missing_filepath = os.path.join(arglist['SAVE'],'%s_BCP_datasetMissing.csv'%(ki))
+#         missing_df.to_csv(missing_filepath, index=False, sep=",", header=True)
+#         return_dict[ki]=complete_df
+#     return(return_dict)
 
 
 def make_components(hei_dict, complete_df):
@@ -293,19 +294,19 @@ def cow_stuff(key, value, data):
     y=value[-1]
     if key == 'hei_dairy':
         if y == 'DOT0100':
-            tmp2=data[y].astype('float')/3
+            tmp2=data[y].astype('float32')/3
             data[key]=cup2oz(tmp+tmp2)
         else:
             print('NO DAIRY MISSING DOT0100, needs to be last in list')
     if key == 'hei_totproteins':
         if y == 'VEG0700':
-            tmp2=data[y].astype('float')*2 # this is normally 1/2
+            tmp2=data[y].astype('float32')*2 # this is normally 1/2
             data[key]=tmp+tmp2
         else:
             print('NO TOTAL PROTEIN MISSING VEG0700, needs to be last in list')
     if key == 'hei_seafoodplantprot':
         if y == 'VEG0700':
-            tmp2=data[y].astype('float')*2
+            tmp2=data[y].astype('float32')*2
             data[key]=tmp+tmp2
         else:
             print('NO SEAFOOD AND PLANT PROTEIN MISSING VEG0700, needs to be last in list')
